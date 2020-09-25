c26f79fb7dfeddc413a919ca49669083eb78f992
sebbwitt <sebastian.khoo26@gmail.com>
2020-09-25 14:23:14 +0800

    Temporary SignupForm

---
+++ application/src/pages/SignUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUpForm from "layouts/SignUpForm";
+
+const SignUp = () => {
+    return (
+        <div style={{margin: "0 auto", width:"1200px", marginTop:"60px", textAlign:"center"}}>
+            <SignUpForm/>
+        </div>
+    )
+}
+
+export default SignUp;

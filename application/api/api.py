79ac0869ddb31dd156d978b0524cd031066d69df
Tedmund Tan <tedtanzp@hotmail.com>
2020-09-25 14:21:48 +0800

    add signup to db

--- application/api/api.py
+++ application/api/api.py
@@ -3,6 +3,7 @@ import json
 from pprint import pprint
 from bson import json_util
 from db import get_db
+import requests
 
 api = Flask(__name__)
 
@@ -29,8 +30,26 @@ def check_customer_exist():
     check = database.find({'NRIC' : nric})
     customer_details = {}
     if check:
-
         output = [doc for doc in check]
         return json.dumps(output, default=json_util.default)
     else:
         return {}
+
+@api.route('/addsignup')
+def add_into_db():
+    customer_details = request.form
+    token = str(request.headers['Authorization'])
+
+    check_status = requests.post('http://techtrek2020.ap-southeast-1.elasticbeanstalk.com/validateForm', header=token, data=customer_details )
+
+    if check_status == '200':
+        
+        check = db.insert_one(customer_details)
+
+        if check :
+            return jsonify({'result' : 'Success'})
+        else:
+            return jsonify({'result' : 'Error'})
+
+    else:
+        return jsonify({'result' : 'Error in adding '})

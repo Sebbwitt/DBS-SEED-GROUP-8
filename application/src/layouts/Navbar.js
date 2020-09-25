241377dc9911d1a0bf1193ea05ca9476c267a4cd
sebbwitt <sebastian.khoo26@gmail.com>
2020-09-25 11:18:35 +0800

    added navbar template layout

---
+++ application/src/layouts/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li>Somepage</li>
+                <li>Somepage</li>
+                <li>Somepage</li>
+                <li>Somepage</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
--- application/src/pages/About.js
+++ application/src/pages/About.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Test from "components/Test";
+import Navbar from "layouts/Navbar";
 
 
 const About = () => {
     return (
         <div>
-            <Test/>
+            <Navbar/>
             About PAGE???
         </div>
     )

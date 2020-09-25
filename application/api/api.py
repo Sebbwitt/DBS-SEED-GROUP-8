b5646ed97629eaa3834d1d448f46d1dacc3f641a
Tedmund Tan <tedtanzp@hotmail.com>
2020-09-25 15:18:39 +0800

    added's evelyn's api.route

--- application/api/api.py
+++ application/api/api.py
@@ -1,17 +1,18 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session 
 import json
 from pprint import pprint
 from bson import json_util
 from db import get_db
 import requests
+from datetime import datetime, timedelta 
 
 api = Flask(__name__)
 
 database = get_db()
 
-@api.route('/')
-def print_hello_world():
-    return {'msg': 'Hello World.'}
+# @api.route('/')
+# def print_hello_world():
+#     return {'msg': 'Hello World.'}
 
 #test to read data to the data base
 @api.route("/test")
@@ -52,3 +53,27 @@ def add_into_db():
 
     else:
         return jsonify({'result' : 'Error in adding '})
+    
+@api.route('/login',methods=['POST'])
+def login():
+    #check if user is logged in by searching if token isset
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=15)
+    session['start time'] = datetime.utcnow()
+
+#this to be at any page in the website (idk if its possible)
+#to detect how much longer that session left
+    if(datetime.utcnow() - session['start time'] < 5):
+        restart_session()
+
+
+@api.route('/', methods=['GET'])
+def restart_session():
+  #check if user needs extension
+  default_option = False
+  user_choice = request.form.get('extension', default_option)
+  if user_choice == True :
+    #GET from http://techtrek2020.ap-southeast-1.elasticbeanstalk.com/extendSession
+    token = requests.get('http://techtrek2020.ap-southeast-1.elasticbeanstalk.com/extendSession')
+    flask.session.modified = True
+    return token

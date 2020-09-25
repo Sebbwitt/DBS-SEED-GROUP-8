f1052278d73758596d949b5ee4bd6baddd7a6668
jitrakorn <39295166+jitrakorn@users.noreply.github.com>
2020-09-25 14:31:33 +0800

    login button

--- application/src/App.js
+++ application/src/App.js
@@ -7,15 +7,21 @@ import Home from "pages/Home";
 import About from "pages/About";
 import Login from "pages/Login";
 
+import Navbar from "layouts/Navbar";
+import Footer from "layouts/Footer";
+
 function App() {
 	return (
-		<Router>
-			<Switch>
-				<Route exact path="/" render={Home}/>
-				<AuthenticatedRoute path="/about" component={About}/>
-				<Route path="/login" component={Login}/>
-			</Switch>
-		</Router>
+		<div>
+			<Router>
+				<Switch>
+					<Route exact path="/" render={Home}/>
+					<Route path="/about" component={About}/>
+					<Route path="/login" component={Login}/>
+				</Switch>
+			</Router>
+		</div>
+
 	);
 }
 

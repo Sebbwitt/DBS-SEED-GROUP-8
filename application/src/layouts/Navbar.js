292b40ac2b82f9521f63ddcbfefaf9a4559e5091
ASUS-UX430Q <e0177138@u.nus.edu>
2020-09-25 15:10:49 +0800

    1) UI refinement 2) auth guard route 3) linking of pages

--- application/src/layouts/Navbar.js
+++ application/src/layouts/Navbar.js
@@ -19,11 +19,11 @@ const Navbar = () => {
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
+                    {/* <li class="nav-item">
                     <Link className="nav-link" to="/home">Home</Link>
-                    </li>
+                    </li> */}
                     <li className="nav-item">
-                        <Link className="nav-link" to="/about">About Us</Link>
+                        <Link className="nav-link" to="/signup">Sign Up</Link>
                     </li>
                     {/* <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
--- application/src/pages/SignUp.js
+++ application/src/pages/SignUp.js
@@ -1,11 +1,20 @@
 import React from "react";
 import SignUpForm from "layouts/SignUpForm";
+import Navbar from "layouts/Navbar";
+import Footer from "layouts/Footer";
 
 const SignUp = () => {
     return (
-        <div style={{margin: "0 auto", width:"1200px", marginTop:"60px", textAlign:"center"}}>
-            <SignUpForm/>
+
+        <div>
+            <Navbar />
+            <div style={{ margin: "0 auto", width: "1200px", marginTop: "60px", textAlign: "center" }}>
+                <SignUpForm />
+            </div>
+            <Footer />
+
         </div>
+
     )
 }
 
